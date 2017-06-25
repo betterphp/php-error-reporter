42a7f0decbc9caa31351443f95f087bc069fbc52
Jacek Kuzemczak <jacek@kuzemczak.co.uk>
2017-06-25 19:30:57 +0100

    Added tests for get url methods on sentry reporter

--- tests/SentryReporterTest.php
+++ tests/SentryReporterTest.php
@@ -32,4 +32,55 @@ class SentryReporterTest extends ReporterTestCase {
         $this->assertSame($expected_project_id, $actual_project_id);
     }
 
+    /**
+     * @dataProvider dataGetReporttUrl
+     */
+    public function testGetReportUrl(array $expected_credentials, string $method_name, string $expected_url): void {
+        $reporter = new sentry_reporter();
+
+        $reporter->set_report_url(...array_values($expected_credentials));
+
+        $this->assertSame($expected_url, $reporter->$method_name());
+    }
+
+    public function dataGetReporttUrl(): array {
+        $creds = [
+            'hostname' => 'very.host',
+            'username' => 'such_user',
+            'password' => 'many_secure',
+            'project_id' => 1337,
+        ];
+
+        return [
+            [
+                $creds,
+                'get_internal_report_url',
+                "https://{$creds['username']}:{$creds['password']}@{$creds['hostname']}/{$creds['project_id']}",
+            ],
+            [
+                $creds,
+                'get_client_report_url',
+                "https://{$creds['username']}@{$creds['hostname']}/{$creds['project_id']}",
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider dataGetReportUrlWithNoUrlSet
+     */
+    public function testGetReportUrlWithNoUrlSet(string $method_name): void {
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('URL has not been set');
+
+        $reporter = new sentry_reporter();
+        $reporter->$method_name();
+    }
+
+    public function dataGetReportUrlWithNoUrlSet(): array {
+        return [
+            ['get_internal_report_url'],
+            ['get_client_report_url'],
+        ];
+    }
+
 }
